b7f873168ce19fc210a08b8bcfd80524b1723a38
alaysiams <alaysiams@gmail.com>
2025-10-09 00:10:30 -0700

    S7

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,6 @@
 import Card from "../components/card.js";
 import FormValidator from "../components/FormValidator.js";
-
-const initialCards = [
-  {
-    name: "Yosemite Valley",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
-  },
-  {
-    name: "Lake Louise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
-  },
-  {
-    name: "Bald Mountains",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
-  },
-  {
-    name: "Latemar",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
-  },
-  {
-    name: "Vanoise National Park",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
-  },
-  {
-    name: "Lago di Braies",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
-  },
-];
-
-const cardData = {
-  name: "Yosemite Valley",
-  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
-};
+import { initialCards } from "../components/card.js";
 
 // Elements //
 const config = {
@@ -75,8 +44,8 @@ cardFormValidator.enableValidation();
 
 // Functions //
 
-function handleImageClick(name, link) {
-  previewImage.src = link;
+function handleImageClick(cardInstance) {
+  previewImage.src = cardInstance._link;
   previewImage.alt = name;
   previewTitle.textContent = name;
   openPopup(previewModal);
@@ -121,11 +90,11 @@ profileEditButton.addEventListener("click", () => {
   profileDescriptionInput.value = profileDescription.textContent;
 
   openPopup(profileEditModal);
-  profileFormValidator.resetValidation();
 });
 
 profileEditCloseButton.addEventListener("click", function () {
   closePopup(profileEditModal);
+  profileFormValidator.resetValidation();
 });
 
 profileEditForm.addEventListener("submit", handleProfileFormSubmit);
